d555be7713e1887ae4109d4e4900f4a473c0698d
vichet vatt <vichetvattedu@gmail.com>
2023-10-29 16:30:09 +0700

    Patch data with express

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const app = express();
+app.use(express.json())
 
-app.use(bodyParser.json());
 
 const toursInfo = JSON.parse(
-    fs.readFileSync('./dev-data/data/tours-simple.json')
+    fs.readFileSync('./dev-data/data/tours-simple.json'));
 
-);
+
+app.get('/', (req, res) => {
+    res.status(200).send("Home Page.");
+});
+
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -18,14 +21,60 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
-var items = [];
-app.post('/api/v1/tours', (req, res) => {
-    var item = req.body;
-    console.log(item);
-    items.push(item);
-    res.status(404).send("Item added successfully.");
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tourInfoByID = toursInfo.find(item => item.id === id);
+
+    // if (id > toursInfo.length) {
+    if (!tourInfoByID) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tourInfoByID,
+        }
+    });
 });
 
+
+app.post('/api/v1/tours', (req, res) => {
+    const newID = toursInfo.length + 1;
+    const newTour = Object.assign({ id: newID, }, req.body);
+    toursInfo.push(newTour);
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(toursInfo), (err, data) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tours: newTour,
+            }
+        });
+    });
+
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tourInfoByID = toursInfo.find(item => item.id === id);
+
+    // if (id > toursInfo.length) {
+    if (!tourInfoByID) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        });
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: '<Updated tour here>'
+        }
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
