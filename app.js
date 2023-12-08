0cb43028895c2f899eb730220388acf87436bb9c
vichet vatt <vichetvattedu@gmail.com>
2023-12-08 23:53:38 +0700

    Handling invalid api route

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const app = express();
 
 console.log(`######## Environment : ${process.env.NODE_ENV} ########`);
 if (process.env.NODE_ENV === 'development') {
-    console.log("Environment Development");
-    app.use(morgan('dev'));
+  console.log('Environment Development');
+  app.use(morgan('dev'));
 }
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(express.static('./public'));
-
 
 //Mounting routers
 app.use('/api/v1/tours', toursRouter);
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
