303531887d9d52607c3e5fd27dbe35c0c2aee792
vichet vatt <vichetvattedu@gmail.com>
2023-12-09 00:25:15 +0700

    Created global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const toursRouter = require('./routes/toursRoute');
 const usersRouter = require('./routes/usersRoute');
 
@@ -20,10 +22,12 @@ app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  //   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  //   err.status = 'fail';
+  //   err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
