dc18694b76478781e02cec7650adb0c0264f5f4a
vichet vatt <vichetvattedu@gmail.com>
2023-12-11 16:33:23 +0700

    Handle duplicated fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,57 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  console.log('err.keyValue.name: ', err.keyValue.name);
+  const message = `Duplicated field value: ${value}. Please use another one`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sentErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    console.log('DEV');
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    console.log('PROD');
+    let error = { ...err };
+    if (err.name === 'CastError') {
+      console.log('TRUE #########################');
+      error = handleCastErrorDB(error);
+    }
+    if (err.code === 11000) {
+      console.log('err.code', err.code);
+      error = handleDuplicateFieldsDB(err);
+    }
+    sentErrorProd(error, res);
+  }
 };
