8626d79ba343a88c5fc670be4caa7cd02e630b7e
vichet vatt <vichetvattedu@gmail.com>
2023-12-12 11:19:20 +0700

    Hash users password

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,7 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-
+    unique: true,
     require: [true, 'Email is required!'],
     lowercase: true,
     trim: true,
@@ -26,8 +27,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
     require: [true, 'Email is required!'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
     minLength: 8,
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //Password Fill is changed or saved
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
