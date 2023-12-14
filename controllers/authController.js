f2553ba1b687daa8653e1b03a0df3a33e403fb6c
vichet vatt <vichetvattedu@gmail.com>
2023-12-14 19:25:14 +0700

    Created Login User

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/usersModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -22,3 +23,15 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(new AppError('Please provide both Email and Password', 400));
+  }
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token: token,
+  });
+};
